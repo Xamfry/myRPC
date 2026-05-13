ead07241a1b20e1bd4b09c5473cc5e2c08474ab6
Xamfry <vlad.kopylov2573@mail.ru>
2026-05-13 14:10:13 -0400

    handle stream clients in worker processes

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC = gcc
-CFLAGS = -Wall -Wextra -Werror -std=c11
+CFLAGS = -Wall -Wextra -Werror -std=c11 -D_POSIX_C_SOURCE=200809L
 
 BIN_DIR = bin
 
--- src/server/main.c
+++ src/server/main.c
@@ -2,15 +2,27 @@
 #include "../common/protocol.h"
 #include "../common/users.h"
 #include <arpa/inet.h>
+#include <errno.h>
 #include <netinet/in.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #define DEFAULT_CONFIG_PATH "config/myRPC.conf"
 #define DEFAULT_USERS_PATH "config/users.conf"
 #define BACKLOG 10
+
+static void handle_sigchld(int signo)
+{
+    (void)signo;
+
+    while (waitpid(-1, NULL, WNOHANG) > 0)
+    {
+    }
+}
 
 static int handle_client(int client_fd, const struct user_list *users)
 {
@@ -39,8 +51,8 @@ static int handle_client(int client_fd, const struct user_list *users)
         return 1;
     }
 
-    printf("request from user: %s\n", request.login);
-    printf("command: %s\n", request.command);
+    printf("worker pid %d: request from user %s\n", getpid(), request.login);
+    printf("worker pid %d: command %s\n", getpid(), request.command);
 
     if (!is_user_allowed(users, request.login))
     {
@@ -51,7 +63,7 @@ static int handle_client(int client_fd, const struct user_list *users)
     }
 
     build_response(response_buffer, sizeof(response_buffer), 0,
-                   "request accepted by server");
+                   "request accepted by worker process");
     send(client_fd, response_buffer, strlen(response_buffer), 0);
 
     return 0;
@@ -65,6 +77,18 @@ static int run_stream_server(int port, const struct user_list *users)
     struct sockaddr_in server_addr;
     struct sockaddr_in client_addr;
     socklen_t client_len;
+    struct sigaction sa;
+
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = handle_sigchld;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+
+    if (sigaction(SIGCHLD, &sa, NULL) < 0)
+    {
+        perror("sigaction");
+        return 1;
+    }
 
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd < 0)
@@ -101,23 +125,46 @@ static int run_stream_server(int port, const struct user_list *users)
         return 1;
     }
 
-    printf("myRPC-server listening on port %d\n", port);
+    printf("myRPC-server parent pid %d listening on port %d\n",
+           getpid(), port);
 
     while (1)
     {
+        pid_t pid;
+
         client_len = sizeof(client_addr);
 
         client_fd = accept(server_fd, (struct sockaddr *)&client_addr,
                            &client_len);
         if (client_fd < 0)
         {
+            if (errno == EINTR)
+            {
+                continue;
+            }
+
             perror("accept");
             continue;
         }
 
         printf("client connected: %s\n", inet_ntoa(client_addr.sin_addr));
 
-        handle_client(client_fd, users);
+        pid = fork();
+        if (pid < 0)
+        {
+            perror("fork");
+            close(client_fd);
+            continue;
+        }
+
+        if (pid == 0)
+        {
+            close(server_fd);
+            handle_client(client_fd, users);
+            close(client_fd);
+            exit(0);
+        }
+
         close(client_fd);
     }
 
